e384fba8f1af520b885e9d39caf2452e2763fa29
Simon Auer <simon@du.digital>
2020-09-17 02:32:47 +0200

    Added examples 10 and 11

---
+++ example11/src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <h1>My name is: {{name}}</h1>
+
+    <input type="text" />
+
+    <button type="text">Speichern</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data: () => ({
+    name: "",
+  }),
+};
+</script>
